d88c0e7fb8a73950cd314c460a6f9bbf6a1abfdb
CarlosGaleana <89460751+CarlosGaleana@users.noreply.github.com>
2022-04-17 13:13:46 -0500

    Add files via upload

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+
+const Carlos = new MissionCommander("Carlos")
+console.log(Carlos.name)
+
+const Daysi = new MissionCommander("Daysi")
+console.log(Daysi.name)
