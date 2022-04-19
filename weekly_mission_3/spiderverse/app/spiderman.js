343931963b716e98dbd619ef16a212fa3b5d26da
CarlosGaleana <89460751+CarlosGaleana@users.noreply.github.com>
2022-04-18 20:57:34 -0500

    Mejora

--- weekly_mission_3/spiderverse/app/spiderman.js
+++ weekly_mission_3/spiderverse/app/spiderman.js
@@ -1,13 +1,13 @@
 class spiderman {
-    constructor (name,age,actor,movies,cinema) {
+    constructor (name,age,actor,movies,studio) {
       this.name = name
       this.age = age
       this.actor = actor
       this.movies = movies
-      this.cinema = cinema
+      this.studio = studio
     }
     getInfo(){ // es una función que ejecutará cualquier objeto instanciado de esta clase
-        return `Hey, I´m  ${this.name} from ${this.cinema} studio`
+        return `Hey, I'm  ${this.actor} from ${this.studio} studio`
       }
   }
   
