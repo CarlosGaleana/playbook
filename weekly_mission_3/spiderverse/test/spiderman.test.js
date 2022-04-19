731a1b721ed9847644e0317516ff7190c2a8a4bd
CarlosGaleana <89460751+CarlosGaleana@users.noreply.github.com>
2022-04-18 20:57:51 -0500

    Updated

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -10,10 +10,10 @@ describe("Unit Tests for Spiderman class", () => {
         expect(andrewGarfield.age).toBe(31)
         expect(andrewGarfield.actor).toBe("Andrew Garfield")
         expect(andrewGarfield.movies).toBe(2)
-        expect(andrewGarfield.cinema).toBe("Sony")
+        expect(andrewGarfield.studio).toBe("Sony")
     });
     test('2) Use the method getInfo()', () => {
         const tomHolland = new spiderman("Spiderman Marvel", 25, "Tom Holland", 5,"Marvel")
-        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
+        expect(tomHolland.getInfo()).toBe("Hey, I'm  Tom Holland from Marvel studio")
     });
   })
