1310f28bf88ab44eda9c86b12f9822c7c81a0f3f
CarlosGaleana <89460751+CarlosGaleana@users.noreply.github.com>
2022-04-25 20:16:52 -0500

    Upload 1

---
+++ weekly_mission_4/fizzbuzz/app2.js
@@ -0,0 +1,22 @@
+// Part 1 Read json file ===========================
+const Reader = require("./lib/utils/Reader");
+const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
+// Aplicación del ExplorerService sobre la lista de explorers
+const ExplorerService = require("./lib/services/ExplorerService");
+ExplorerService.filterByMission(explorers, "node");
+ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+
+//Validaciones
+const FizzbuzzService = require("./lib/services/FizzbuzzService");
+const explorer1 = {name: "Explorer1", score: 1}
+FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
